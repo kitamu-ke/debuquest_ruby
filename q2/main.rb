48408ad4d9fb5fcff6bb80abf29713d9e157ca3e
KitamuraKen <k.ken0324.soccer2610@gmail.com>
2023-05-21 13:17:56 +0900

    文字列が正しく出るように条件分岐を整えた

--- q2/main.rb
+++ q2/main.rb
@@ -6,10 +6,11 @@ class Q2::Main < Base
     puts "お名前を入力してください"
 
     name = gets.chomp
-    is_name_string = !name.match(/[0-9]+/)
-    return puts "文字列で入力してください" if is_name_string
-
+    if name.match(/[0-9]+/)
+    return puts "文字列で入力してください"
+    else
     user = User.new(name)
     user.self_introduce
+    end
   end
 end
--- q2/user.rb
+++ q2/user.rb
@@ -1,9 +1,12 @@
 class User
+
+  attr_accessor :name
+
   def initialize(name)
     @name = name
   end
 
   def self_introduce
-    puts "私の名前は #{name} です！！"
+    puts "私の名前は #{@name} です！！"
   end
 end
