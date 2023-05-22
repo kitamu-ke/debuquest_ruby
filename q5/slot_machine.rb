28f3a749dd98a23385235e45522c25e815b87b70
KitamuraKen <k.ken0324.soccer2610@gmail.com>
2023-05-22 21:52:44 +0900

    スロットが回るように修正

--- q5/slot_machine.rb
+++ q5/slot_machine.rb
@@ -16,11 +16,11 @@ class Q5::SlotMachine
     same_numbers_count = count_same_numbers(number1: number1, number2: number2, number3: number3)
     puts "結果: |#{number1}|#{number2}|#{number3}|"
     case same_numbers_count
-    when 1
+    when 0
       puts "残念！ハズレ！"
-    when 2
+    when 1
       puts "惜しい！"
-    when 3
+    when 2
       puts "大当たり！"
     end
   end
@@ -28,7 +28,7 @@ class Q5::SlotMachine
   private
 
     def count_same_numbers(number1:, number2:, number3:)
-      same_numbers_count = if (number1 == number2 && number1 == number3)
+      same_numbers_count = if  (number1 == number2 && number1 == number3)
                             2
                           elsif number1 == number2 || number1 == number3 || number2 == number3
                             1
